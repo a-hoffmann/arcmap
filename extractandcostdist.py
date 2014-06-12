affc1ded2d363e4b04d4ecf5ca79b6095dbe9feb
a-hoffmann <alexandr.hoffmann@gmail.com>
2014-06-12 10:23:21 +0200

    Yo fred here are my lunch-break modifs

--- extractandcostdist.py
+++ extractandcostdist.py
@@ -13,12 +13,12 @@ cursor=arcpy.SearchCursor(fc)
 for row in cursor:
 	FID = str(row.getValue("FID"))
 	where = '"FID" =' + FID
-	out_fl='fl_{0}'.format(FID)
+	out_fl='fl_{0}'.format(FID) #not big on this, whats the .format( ) doing?: could be right, I haven't seen something like that in Python, check that it produces what you expect
+	print out_fl
 	for FID in range(0,1000): #need to break into segments because of unexpected crashing
 		arcpy.MakeFeatureLayer_management(fc,out_fl,where)
 		arcpy.sa.CostDistance(out_fl,cost,100000).save('codist_{0}'.format(FID)) #1000000 max cost dist chosen out of convenience (covers area needed)
 		arcpy.Delete_management(out_fl) #delete useless files in process, clears up space and runs smoother
 		print 'done_{0}'.format(FID) #printing this helps keep track of how much has been run
-	else:
-		print 'rangedone'
-		break
+	#this else isn't meaningful here: all rows are going to be in cursor anyway! Instead you can just dedent the code
+print 'rangedone'
